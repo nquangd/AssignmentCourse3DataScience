d17c0bc405a531c4f1a6abf4c6d69a0062356b47
nquangd <nquangd@gmail.com>
2018-10-15 22:20:38 +0200

    Finalise the assignment

---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+## This is the script to tidy up the data collected by the activity bands
+
+The script does the following steps:
+
+	* Read the raw collected data, the subject id, and the activity id using read.csv
+	* Add the subject id and activity id to the data set using cbind. This is done for both Train and Test data set
+	* Merge Train and Test data set using rbind
+	* Get the id for the variables by using the grep command to look for the pattern std() and mean()
+	* Extract the merge data set by using the variables id obtained from previous step to pass to the column index of the data set
+	* Using sapply to label the activity by matching the activity id in the data set and the activity list
+	* Rename the whole data set using the variables names obtained from step 4
+	* Calculate the average for each subject - activity combination by aggregate command
+	
+The data set, the variables and the transformation steps are described in the CodeBook
+
+
+To use this script, run
+
+> run_analysis()  in the R command prompt
+
+this will write a tidy data set named tidydata.csv
+
---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+run_analysis <- function() {
+	
+	## Read data file in train
+	xtr <- read.csv("./train/X_train.txt",sep="",header=FALSE)
+	ytr <- read.csv("./train/y_train.txt",sep="",header=FALSE)
+	sjtr <- read.csv("./train/subject_train.txt",sep="",header=FALSE)
+	
+	# add the subject id and activity id as columns to the data set
+	train <- cbind(sjtr,ytr,xtr)
+	
+	## Read data file in test
+	xt <- read.csv("./test/X_test.txt",sep="",header=FALSE)
+	yt <- read.csv("./test/y_test.txt",sep="",header=FALSE)
+	sjt <- read.csv("./test/subject_test.txt",sep="",header=FALSE)
+	
+	# add the subject id and activity id as columns to the data set
+	test <- cbind(sjt,yt,xt)
+	
+	## Read the feature list
+	f <- read.csv("./features.txt",sep="",header=FALSE)
+	
+	## Read the activity label
+	a <- read.csv("./activity_labels.txt",sep="",header=FALSE)
+	
+	## STEP 1: Merge the data
+	
+	data <- rbind(train,test)
+	
+	## STEP 2: Extract only the std() and mean() for each measurement
+	
+	## Get the code for the std() and mean() of each feature in the feature list
+	f <- f[grep("mean\\()|std\\()",f[,2]),]    ## Use \\ so that R recognises () as literal character
+	
+	## Extract the data
+	data <- cbind(data[,1:2],data[,f[,1]+2])    ## "+2" since two columns were added to the data
+	
+	## STEP 3: Add descriptive name for activity in the merged data set. Retrieve from the label df a
+	
+	data[,2] <- sapply(data[,2], function(x) a[x,2])
+	
+	## STEP 4: Add the descriptive name for the variables
+	
+	f[,2] <- sub("^t","time",f[,2])  			## replace "t" and "f" to something selfexplanatory
+	f[,2] <- sub("^f","frequency",f[,2])
+	
+	names(data)[3:length(names(data))] <- f[,2]   ## rename the names of the data set
+	names(data)[1:2] <- c("Subject","Activity")
+	
+	## STEP 5: Find the average for each subject and each activity
+	
+	data <- aggregate(data[3:dim(data)[2]], by = list(data$Subject,data$Activity), FUN = function(x) mean(x))
+	names(data)[1:2] <- c("Subject","Activity")
+	
+	write.csv(data, file = "tidydata.csv", row.names=FALSE)
+	
+}
